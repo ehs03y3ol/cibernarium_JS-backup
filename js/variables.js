02c85bc240078ff04424845095a8e950a0ae24cc
Armando <47246587+armandolap@users.noreply.github.com>
2019-11-08 11:06:51 +0100

    8/11/19 11:00

---
+++ js/variables.js
@@ -0,0 +1,12 @@
+// VARIABLES
+// def: celdas de memoria donde podemos
+// almacenar diferentes tipos de datos
+// --------
+// declaracion de variables y asignación de datos
+var x = 5; //tipo de dato entero
+var y = 2.7; //tipo de dato decimal (float)
+var texto = "Hola"; //tipo de dato texto (string)
+var texto2 = 'Hola'; //tipo de dato texto (string)
+var boolean = true; //tipo de dato booleano verdadero (true)
+y = "hola";
+alert(y);
