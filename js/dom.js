1af2a54c7349c0c480c61a1e30965591ef5b49c0
Armando <47246587+armandolap@users.noreply.github.com>
2019-11-14 13:14:15 +0100

    14/11/19 final

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,21 @@
-DOM: Document Object Model
-Def: Nos permite almacenar, cambiar, añadir o eliminar
-cualquier elemento de nuestra web
+// DOM: Document Object Model
+// Def: Nos permite almacenar, cambiar, añadir o eliminar
+// cualquier elemento de nuestra web
+
+// almaceno en una variable el elemento (objeto)
+// con id='logo' (imagen)
+var logo = document.getElementById('logo');
+// alert(logo);
+// añadimos un borde rojo al logo
+logo.style.border = "2px red solid";
+
+// por clase
+var imagenes = document.getElementsByClassName('imagen');
+// alert(imagenes); //array
+
+// pinto el borde de la seguna imagen
+// imagenes[1].style.border = "2px solid red";
+// pintamos todas las imagenes
+for (var i = 0; i < imagenes.length; i++) {
+    imagenes[i].style.border = "2px solid red";
+}
---
+++ js/validacion.js
@@ -0,0 +1,11 @@
+function validar(){
+    var nombre = document.getElementById('nombre');
+    if(nombre.value == ""){
+        alert("Nombre vacío");
+    }else{
+        alert("OK");
+    }
+
+
+
+}
