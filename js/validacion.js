6c85164ee4b59d75748266ef4c6ae756e54127e7
Armando <47246587+armandolap@users.noreply.github.com>
2019-11-22 13:28:28 +0100

    FINAL

--- js/validacion.js
+++ js/validacion.js
@@ -7,6 +7,7 @@ function validar(){
     // cogemos por ID los campos
     var nombre = document.getElementById('nombre');
     var email = document.getElementById('email');
+    var tel = document.getElementById('tel');
     // cogemos por nombre de etiqueta (array)
     var mensaje = document.getElementsByTagName('textarea');
 
@@ -31,6 +32,19 @@ function validar(){
     // si no se cumple ninguna de las condiciones anteriores
     }else{
         email.style.border = "1px solid #aaa";
+    }
+    // VALID. TELEFONO
+    if(tel.value == ""){
+        tel.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono está vacío</h3>";
+    }else if(isNaN(tel.value)){
+        tel.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono no es numérico</h3>";
+    }else if(!validatePhone(tel.value)){
+        tel.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono no es correcto</h3>";
+    }else{
+        tel.style.border = "1px solid #aaa";
     }
     // MENSAJE (valida campo mensaje vacío y además que el usuario añada mas de 10 caracteres)
     // tenemos que indicar que cogemos el primer elemento (textarea) ya que es un array
@@ -59,3 +73,34 @@ function validateEmail(email){
 // comprobacion
 // var result_email = validateEmail("armand@gmail.c");
 // alert(result_email);
+
+// funcion que valida con RegExp un telefono
+function validatePhone(phone){
+    var regExp = /^[9|7|6]{1}([\d]{2}[-]*){3}[\d]{2}$/;
+    return regExp.test(phone);
+}
+// FUNCION QUE VALIDA CON EVENTO Keyup
+function validar_keyup(){
+
+    var mensajes ="";
+    var tel1 = document.getElementById('tel1');
+    //cogemos por ID la caja de mensajes de error "results"
+    var results1 = document.getElementById('results1');
+
+    if(tel1.value == ""){
+        tel1.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono está vacío</h3>";
+    }else if(isNaN(tel1.value)){
+        tel1.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono no es numérico</h3>";
+    }else if(!validatePhone(tel1.value)){
+        tel1.style.border = "1px solid red";
+        mensajes += "<h3>El campo telefono no es correcto</h3>";
+    }else{
+        tel1.style.border = "1px solid #aaa";
+    }
+
+    // inyecto en la caja los mensajes de error de validacion
+    results1.innerHTML = mensajes;
+
+}
